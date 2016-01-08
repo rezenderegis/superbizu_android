31b67aa8475365bc8e9974ad31e391e18efd0c56
fabricio <rezenderegis@gmail.com>
2016-01-07 22:41:27 -0200

    Salvando as questões do servidor

--- app/src/main/java/com/bizu/question/AsyncQuestionRepository.java
+++ app/src/main/java/com/bizu/question/AsyncQuestionRepository.java
@@ -8,6 +8,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 import android.provider.BaseColumns;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +21,37 @@ public class AsyncQuestionRepository extends SQLiteOpenHelper implements Questio
     public final static String DATABASE_NAME = "Question.db";
     public final static int DATABASE_VERSION = 1;
 //    private static final String COMMA_SEP = ",";
+
+    //Definição de atributos da TB_QUESTAO
+    public static final String TB_QUESTAO = "TB_QUESTAO";
+
+    public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+
+    public static final String ID_QUESTAO = "ID_QUESTAO";
+
+    public static final String ANO_QUESTAO = "ANO_QUESTAO";
+
+    public static final String NUMERO_QUESTAO = "NUMERO_QUESTAO";
+
+    public static final String DESCRICAO_QUESTAO = "DESCRICAO_QUESTAO";
+
+    public static final String COMANDO_QUESTAO = "COMANDO_QUESTAO";
+
+    public static final String PROVA = "PROVA";
+
+    public static final String SITUACAO_QUESTAO = "SITUACAO_QUESTAO";
+
+    public static final String IMAGEM_QUESTAO = "IMAGEM_QUESTAO";
+
+    public static final String COMENTARIO_QUESTAO = "COMENTARIO_QUESTAO";
+
+    public static final String LETRA_ITEM_CORRETO = "LETRA_ITEM_CORRETO";
+
+    public static final String DIA_PROVA = "DIA_PROVA";
+
+    public static final String APLICACAO = "APLICACAO";
+
+
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + QuestionContract.TABLE_NAME + " (" +
                     QuestionContract._ID + " INTEGER PRIMARY KEY" +
@@ -26,6 +59,9 @@ public class AsyncQuestionRepository extends SQLiteOpenHelper implements Questio
 
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + QuestionContract.TABLE_NAME;
+
+
+
 
     public static abstract class QuestionContract implements BaseColumns {
         public static final String TABLE_NAME = "question";
@@ -50,12 +86,17 @@ public class AsyncQuestionRepository extends SQLiteOpenHelper implements Questio
                 "  ID_QUESTAO INT NOT NULL," +
                 "  ANO_QUESTAO INT NULL," +
                 "  NUMERO_QUESTAO INT NULL," +
-                "  DESCRICAO_QUESTAO VARCHAR(2000) NULL," +
-                "  COMANDO_QUESTAO VARCHAR(2000) NULL," +
+                "  DESCRICAO_QUESTAO VARCHAR(3000) NULL," +
+                "  COMANDO_QUESTAO VARCHAR(3000) NULL," +
                 "  PROVA INT NULL," +
                 "  SITUACAO_QUESTAO INT NULL," +
                 "  IMAGEM_QUESTAO VARCHAR(100) NULL," +
+                "  COMENTARIO_QUESTAO VARCHAR(3000) NULL," +
+                "  LETRA_ITEM_CORRETO VARCHAR(1) NULL," +
+                "  DIA_PROVA INT(1) NULL," +
+                "  APLICACAO INT(1) NULL," +
                 "  PRIMARY KEY (ID_QUESTAO));";
+
 
         String ddlItem = "CREATE TABLE IF NOT EXISTS TB_ITEM (" +
                 "  ID_ITEM INT NOT NULL," +
@@ -150,6 +191,38 @@ public class AsyncQuestionRepository extends SQLiteOpenHelper implements Questio
 
 
 
+
+    public void saveQuestion(List<Question> questions) {
+
+        ContentValues contentValues = new ContentValues();
+
+        try {
+
+            for (Question question : questions) {
+
+                contentValues.put(ID_QUESTAO, question.getId());
+                contentValues.put(DESCRICAO_QUESTAO, question.getDescricao_questao());
+                contentValues.put(ANO_QUESTAO, question.getAno_questao());
+                contentValues.put(NUMERO_QUESTAO, question.getNumero_questao());
+                contentValues.put(COMANDO_QUESTAO, question.getComando_questao());
+                contentValues.put(PROVA, question.getProva());
+                contentValues.put(SITUACAO_QUESTAO, question.getSituacao_questao());
+                contentValues.put(IMAGEM_QUESTAO, question.getImagem_questao());
+                contentValues.put(COMENTARIO_QUESTAO, question.getComentario_questao());
+                contentValues.put(DIA_PROVA, question.getDia_prova());
+                contentValues.put(APLICACAO, question.getAplicacao());
+                getWritableDatabase().insert(TB_QUESTAO, null, contentValues);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
 }
 
 class AsyncSave extends AsyncTask<Question, Integer, Question> {
@@ -172,3 +245,5 @@ class AsyncSave extends AsyncTask<Question, Integer, Question> {
         mListener.onSaveFinish(question);
     }
 }
+
+
